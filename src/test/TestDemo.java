71a30f1387d0812e56138b73d135f49b5b0b71f5
zhoudunyang <zhoudunyang@xingcloud.com>
2013-07-30 11:59:04 +0800

    add  test demo
    test demo

---
+++ src/test/TestDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Administrator
+ *
+ */
+public class TestDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("heihei~");
+	}
+
+}
